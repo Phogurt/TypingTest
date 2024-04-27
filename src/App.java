695293a5cec8c5a2bf9ab2b38e6854c102cd5c4c
Beetooo <betosantospyne@gmail.com>
2024-04-27 19:10:39 -0400

    completed basic structure

--- src/App.java
+++ src/App.java
@@ -1,7 +1,57 @@
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.event.*;  
 
 public class App {
+    static Timer timer;
+    static JTextField t;
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int width=400,height=400;
+        JFrame f=new JFrame();
+        JLabel l1= new JLabel("Welcome to our typing test. Press any key to start.");
+        l1.setBounds(width/2-10-150,50,300,30);
+        
+        t=new JTextField();
+        t.setSize(width-50,50);
+        t.setLocation(15,100);
+
+        JButton startButton=new JButton("Restart");  
+        startButton.setBounds(width/2-10-50,height-100, 100,30);
+
+        startTimer();
+        startButton.addActionListener(new ActionListener(){  
+            public void actionPerformed(ActionEvent e){
+                //checking if test already started so we dont start it twice
+                if(timer.isRunning()){
+                    timer.stop();
+                }
+                //10 second timer
+                t.setText("");
+                startTimer();
+            }
+        });
+        
+
+
+        f.add(l1);
+        f.add(t);
+        f.add(startButton);
+        f.setSize(width,height);
+        f.setLayout(null);  
+        f.setVisible(true);
+        f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
     }
+    public static void startTimer(){
+        timer = new Timer(10000, new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                testOver(t.getText());
+            }
+        });
+        timer.setRepeats(false);
+        timer.start();
+    }
+    public static void testOver(String text){
+        System.out.println(text);
+    }
+    
 }
